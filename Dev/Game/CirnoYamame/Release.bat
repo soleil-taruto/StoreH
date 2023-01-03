dbb9497ad2e8a3aec5904d51d96971a747f61758
soleil.taruto <soleil.taruto@gmail.com>
2023-01-03 21:02:34 +0900

    Voyager 1 is 23734997397.529 kilometers away from the Sun.

--- Dev/Game/CirnoYamame/Release.bat
+++ Dev/Game/CirnoYamame/Release.bat
@@ -2,20 +2,14 @@ CALL C:\Factory\SetEnv.bat
 CALL Clean.bat
 cx **
 
-C:\apps\ConfuserForElsa\ConfuserForElsa.exe Elsa20200001\Elsa20200001.sln C:\temp
-
 C:\Factory\SubTools\makeDDResourceFile.exe ^
 	dat ^
 	out\Resource.dat ^
 	C:\Factory\Program\MaskGZDataForElsa2\MaskGZData_2022103525.exe
 
-COPY /B Elsa20200001\Elsa20200001\bin\Release\Elsa20200001.exe-confused out\Game.exe
+COPY /B Elsa20200001\Elsa20200001\bin\Release\Elsa20200001.exe out\Game.exe
 COPY /B Elsa20200001\Elsa20200001\bin\Release\DxLib.dll out
 COPY /B Elsa20200001\Elsa20200001\bin\Release\DxLibDotNet.dll out
-
-C:\apps\RingCipher\RingCipher.exe /P 6739279112893916687656926480544717157319 /E ^
-	Elsa20200001\Elsa20200001\bin\Release\Elsa20200001.exe-confused-rename-table.txt.gz ^
-	out\rt_dat
 
 xcp doc out
 
