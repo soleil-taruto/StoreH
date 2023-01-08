7352ccd2b4dcd525020a3e9d2fc53c31b36076ad
soleil.taruto <soleil.taruto@gmail.com>
2023-01-08 13:28:37 +0900

    Voyager 1 is 23741834362.925 kilometers away from the Sun.

--- Dev/Program/Hatena20230108/Claes20200001/Claes20200001/Tests/Test0001.cs
+++ Dev/Program/Hatena20230108/Claes20200001/Claes20200001/Tests/Test0001.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Drawing;
 using Charlotte.Commons;
+using Charlotte.Utilities;
 
 namespace Charlotte.Tests
 {
@@ -11,7 +12,20 @@ namespace Charlotte.Tests
 	{
 		public void Test01()
 		{
-			// none
+			LoadXMLTest(@"C:\temp\FG-GML-533935-ALL-20221001\FG-GML-533935-AdmArea-20221001-0001.xml");
+			LoadXMLTest(@"C:\temp\FG-GML-533935-ALL-20221001\FG-GML-533935-BldL-20221001-0001.xml");
+			LoadXMLTest(@"C:\temp\FG-GML-533935-ALL-20221001\FG-GML-533935-RailCL-20221001-0001.xml");
+		}
+
+		private List<XMLNode> LoadedXMLs = new List<XMLNode>();
+
+		private void LoadXMLTest(string file)
+		{
+			Console.WriteLine("Load " + file);
+			DateTime stTm = DateTime.Now;
+			LoadedXMLs.Add(XMLNode.LoadFromFile(file));
+			DateTime edTm = DateTime.Now;
+			Console.WriteLine("OK " + (edTm - stTm).TotalSeconds.ToString("F3") + "s");
 		}
 	}
 }
