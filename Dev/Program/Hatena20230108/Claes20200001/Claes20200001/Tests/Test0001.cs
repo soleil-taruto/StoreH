b1bbe71e034ccf82d0680643e237f7aaadf6d7f8
soleil.taruto <soleil.taruto@gmail.com>
2023-01-08 17:43:27 +0900

    Voyager 1 is 23742092629.733 kilometers away from the Sun.

--- Dev/Program/Hatena20230108/Claes20200001/Claes20200001/Tests/Test0001.cs
+++ Dev/Program/Hatena20230108/Claes20200001/Claes20200001/Tests/Test0001.cs
@@ -93,7 +93,7 @@ namespace Charlotte.Tests
 			ExportPosList(
 				@"C:\temp\FG-GML-533935-ALL-20221001\FG-GML-533935-RdEdg-20221001-0001.xml",
 				@"C:\temp\Road.txt",
-				"Dataset/RailCL/loc/Curve/segments/LineStringSegment/posList"
+				"Dataset/RdEdg/loc/Curve/segments/LineStringSegment/posList"
 				);
 		}
 
@@ -119,11 +119,96 @@ namespace Charlotte.Tests
 			double ER = 6367444.5; // 地球の半径(meter)
 			double LAT = 35.6; // 基準とする北緯(degree)
 
-			double latToMeter = ER * 2.0 * Math.PI / 360.0;
-			double lonToMeter = ER * Math.Cos(LAT * Math.PI / 180.0) * 2.0 * Math.PI / 360.0;
+			double latToMeter = ER * Math.PI / 180.0;
+			double lonToMeter = ER * Math.Cos(LAT * Math.PI / 180.0) * Math.PI / 180.0;
 
 			Console.WriteLine("緯度の 1 度を " + latToMeter.ToString("F9") + " メートルとする。");
 			Console.WriteLine("経度の 1 度を " + lonToMeter.ToString("F9") + " メートルとする。");
+		}
+
+		public void Test05()
+		{
+			// 表示範囲(緯度経度)
+			double NORTH_LAT = 35.61; // 自由が丘駅のちょい上(北)
+			double SOUTH_LAT = 35.595; // 田園調布駅のちょい下(南)
+			double WEST_LON = 139.66; // 九品仏駅のちょい左(西)
+			double EAST_LON = 139.687; // 大岡山駅のちょい右(東)
+
+			// 出力画像の幅
+			int IMAGE_WIDTH = 1600;
+
+			OutputImage(NORTH_LAT, SOUTH_LAT, WEST_LON, EAST_LON, IMAGE_WIDTH);
+		}
+
+		private void OutputImage(double nLat, double sLat, double wLon, double eLon, int imageWidth)
+		{
+			double LAT_TO_METER = 111132.871463004;
+			double LON_TO_METER = 90362.222363910;
+
+			double xLon = eLon - wLon;
+			double yLat = nLat - sLat;
+
+			double xMeter = xLon * LON_TO_METER;
+			double yMeter = yLat * LAT_TO_METER;
+
+			double meterToPixel = imageWidth / xMeter;
+
+			double latToPixel = LAT_TO_METER * meterToPixel;
+			double lonToPixel = LON_TO_METER * meterToPixel;
+
+			int w = imageWidth;
+			int h = SCommon.ToInt(yMeter * meterToPixel);
+
+			Bitmap image = new Bitmap(w, h);
+
+			using (Graphics g = Graphics.FromImage(image))
+			{
+				g.FillRectangle(new SolidBrush(Color.White), 0, 0, w, h);
+
+				Action<string, Color> drawPolyFromFile = (polyFile, color) =>
+				{
+					string[] polyLines = File.ReadAllLines(polyFile, Encoding.ASCII);
+					int polyLineIndex = 0;
+
+					while (polyLineIndex < polyLines.Length)
+					{
+						List<double[]> poly = new List<double[]>();
+
+						while (polyLines[polyLineIndex] != "")
+							poly.Add(polyLines[polyLineIndex++].Split(' ').Select(v => double.Parse(v)).ToArray());
+
+						polyLineIndex++;
+
+						foreach (double[] p in poly)
+						{
+							p[0] = (nLat - p[0]) * latToPixel; // Lat to pixel
+							p[1] = (p[1] - wLon) * lonToPixel; // Lon to pixel
+						}
+
+						for (int polyIndex = 0; polyIndex < poly.Count; polyIndex++)
+						{
+							double[] p = poly[polyIndex];
+							double[] q = poly[(polyIndex + 1) % poly.Count];
+
+							if (
+								(0.0 <= p[0] && p[0] < h) ||
+								(0.0 <= p[1] && p[1] < w) ||
+								(0.0 <= q[0] && q[0] < h) ||
+								(0.0 <= q[1] && q[1] < w)
+								)
+							{
+								g.DrawLine(new Pen(new SolidBrush(color)), (float)p[1], (float)p[0], (float)q[1], (float)q[0]);
+							}
+						}
+					}
+				};
+
+				drawPolyFromFile(@"C:\temp\Building.txt", Color.FromArgb(128, 255, 128));
+				drawPolyFromFile(@"C:\temp\Road.txt", Color.FromArgb(128, 128, 128));
+				drawPolyFromFile(@"C:\temp\Rail.txt", Color.FromArgb(0, 0, 0));
+				drawPolyFromFile(@"C:\temp\Area.txt", Color.FromArgb(255, 64, 0));
+			}
+			image.Save(SCommon.NextOutputPath() + ".png");
 		}
 	}
 }
