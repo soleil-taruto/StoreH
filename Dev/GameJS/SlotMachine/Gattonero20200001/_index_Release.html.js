b5aac1d79724aa28011940390d3b0fca36b63b05
soleil.taruto <soleil.taruto@gmail.com>
2023-01-14 11:54:38 +0900

    Voyager 1 is 23750495361.484 kilometers away from the Sun.

--- Dev/GameJS/SlotMachine/Gattonero20200001/_index_Release.html.js
+++ Dev/GameJS/SlotMachine/Gattonero20200001/_index_Release.html.js
@@ -35,6 +35,41 @@ window.onload = function()
 
 <br/>
 
+遊び方：<br/>
+最初にレーンを選んで下さい。レーンによって絵柄毎の割合が違います。<br/>
+----<br/>
+RESET : ベットしたコインをゼロに戻します。<br/>
+[00] : ここをタップするとコインをベットします。押しっぱなしで連続投入します。<br/>
+AutoBet : 前回と同じ枚数コインを投入します。<br/>
+START : ドラムの回転をスタートします。<br/>
+STOP : 回転しているドラムを止めます。<br/>
+EXIT : タイトルに戻ります。<br/>
+
+<br/>
+
+<div style="color: #a00000;">
+レート<br/>
+====<br/>
+7 BONUS 青 : x 10000<br/>
+7 BONUS 赤 : x 3000<br/>
+BAR : x 1000<br/>
+スイカ 青 : x 300<br/>
+スイカ 赤 : x 100<br/>
+ベル 青 : x 30<br/>
+ベル 黄 : x 10<br/>
+さくらんぼ 緑 : x 3<br/>
+さくらんぼ 赤 : x 2<br/>
+</div>
+
+<br/>
+
+テスト用機能：<br/>
+none<br/>
+
+<br/>
+
+<!--
+
 効果音：<br/>
 　小森平<br/>
 　https://taira-komori.jpn.org/<br/>
@@ -45,15 +80,7 @@ window.onload = function()
 　魔王魂<br/>
 　https://maou.audio/<br/>
 
-<br/>
-
-注意：<br/>
-none<br/>
-
-<br/>
-
-テスト用機能：<br/>
-none<br/>
+-->
 
 <br/>
 <br/>
