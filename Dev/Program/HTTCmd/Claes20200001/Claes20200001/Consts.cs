5518165c3809a947d63039e3417b36f2bf868ab4
soleil.taruto <soleil.taruto@gmail.com>
2023-04-03 09:04:05 +0900

    Voyager 1 is 23865613661.890 kilometers away from the Sun.

--- Dev/Program/HTTCmd/Claes20200001/Claes20200001/Consts.cs
+++ Dev/Program/HTTCmd/Claes20200001/Claes20200001/Consts.cs
@@ -8,6 +8,6 @@ namespace Charlotte
 {
 	public static class Consts
 	{
-		public const string SERVER_STOP_EVENT_NAME = "HTTCmd_STOP_SERVER_{bf862c09-65a5-4c5d-bfde-ef9d987ed5e5}";
+		public const string SERVER_STOP_EVENT_NAME = "HTTCmd_STOP_SERVER_{b6288-M4t3DOKK2PlADSV3eb8xgQ-7jUU04yOCD2wvEY5C3L7Vs-2zRv5bDipG9WyiE8c177E6-jlnj2JSSXAsdREyGUSsjtC}";
 	}
 }
--- Dev/Program/HTTCmdP/Claes20200001/Claes20200001/Consts.cs
+++ Dev/Program/HTTCmdP/Claes20200001/Claes20200001/Consts.cs
@@ -8,6 +8,6 @@ namespace Charlotte
 {
 	public static class Consts
 	{
-		public const string SERVER_STOP_EVENT_NAME = "HTTCmdP_STOP_SERVER_{c3d353ea-e910-4e7d-8598-92b435c5cf53}";
+		public const string SERVER_STOP_EVENT_NAME = "HTTCmdP_STOP_SERVER_{b6288-CeA2B0y5n0nuN8UdNnxlbt-jHhrRKIs9rhvoWkXkKU9Gx-s778XXFIqVPx1ERLiCvJye-pu32J0qvGTg8ClSTCAJCJa}";
 	}
 }
